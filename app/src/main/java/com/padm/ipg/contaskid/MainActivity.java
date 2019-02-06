6964d225ebdfed7376961bfe2c43f02bfc5e6b7c
lpimentel <lamp07@gmail.com>
2019-02-06 11:40:15 +0000

    Implementa funcionalidade de imagens aleatórias em cada início da aplicação.

--- app/src/main/java/com/padm/ipg/contaskid/MainActivity.java
+++ app/src/main/java/com/padm/ipg/contaskid/MainActivity.java
@@ -1,13 +1,60 @@
 package com.padm.ipg.contaskid;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //** Declaração dos objetos para cada campo **//
+
+    private EditText et_nome;
+    private ImageView iv_personagem;
+    private TextView tv_bestscore;
+    private MediaPlayer mp;
+
+    //** Declaração de variavel tipo inteiro com casting **//
+
+    int num_aleatorio = (int) (Math.random() * 10);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //** Relação entre a parte lógica e gráfica **//
+
+        et_nome = (EditText)findViewById(R.id.txt_nome);
+        iv_personagem = (ImageView)findViewById(R.id.imageView_Personagem);
+        tv_bestscore = (TextView)findViewById(R.id.textView_BestScore);
+
+
+        //** Coloca icons na barra action bar **//
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        //** Condição para números **//
+
+        int id;
+        if (num_aleatorio == 0 || num_aleatorio == 10){
+            id = getResources().getIdentifier("ini1", "drawable", getPackageName());
+            iv_personagem.setImageResource(id);
+        }   else if (num_aleatorio == 1 || num_aleatorio == 9){
+            id = getResources().getIdentifier("ini2", "drawable", getPackageName());
+            iv_personagem.setImageResource(id);
+        }   else if (num_aleatorio == 2 || num_aleatorio == 8){
+            id = getResources().getIdentifier("ini3", "drawable", getPackageName());
+            iv_personagem.setImageResource(id);
+        }   else if (num_aleatorio == 3 || num_aleatorio == 7){
+            id = getResources().getIdentifier("ini4", "drawable", getPackageName());
+            iv_personagem.setImageResource(id);
+        }   else if (num_aleatorio == 4 || num_aleatorio == 5 || num_aleatorio == 6){
+            id = getResources().getIdentifier("ini5", "drawable", getPackageName());
+            iv_personagem.setImageResource(id);
+        }
     }
 }
